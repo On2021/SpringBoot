011fbd51bca59a389edfd4589342229ec183eb1b
On2021 <dhkdrnfl4578@knou.ac.kr>
2022-08-04 22:23:03 +0900

    2022.08.04 채팅서버 만들기 5강

---
+++ src/main/java/com/cos/chatapp/ChatController.java
@@ -0,0 +1,35 @@
+package com.cos.chatapp;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+@RequiredArgsConstructor
+@RestController //데이터 리턴 서버
+public class ChatController {
+
+	private final ChatRepository chatRepository;
+	
+	//SSE프로토콜 : 데이터를 요청 받으면 계속 지속적으로 데이터를 보내줄 수 있습니다. (Response선이 끊기지 않고 데이터 전송 가능) => return 타입 Flux
+	@GetMapping(value = "/sender/{sender}/receiver/{receiver}",produces=MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) { //데이터 여러개 리턴할거면 Flux
+		return chatRepository.mFindBySender(sender, receiver)
+				.subscribeOn(Schedulers.boundedElastic());
+	}
+	
+	@PostMapping("/chat")
+	public Mono<Chat> setMsg(@RequestBody Chat chat){ //데이터 한건 리턴할거면 Mono / void 사용해도 됨 => 근데 save 데이터 잘 들어오는지 확인하고 싶어서 이렇게 작성함
+		chat.setCreatedAT(LocalDateTime.now());
+		return chatRepository.save(chat); 
+	}
+}
