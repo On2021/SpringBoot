8cd7cd8e81121b4b8748fe11dba3a3ca648f219a
On2021 <dhkdrnfl4578@knou.ac.kr>
2022-08-06 20:51:55 +0900

    2022.08.06 채팅서버 만들기 10강

--- src/main/java/com/cos/chatapp/ChatController.java
+++ src/main/java/com/cos/chatapp/ChatController.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,15 +23,18 @@ public class ChatController {
 	private final ChatRepository chatRepository;
 	
 	//SSE프로토콜 : 데이터를 요청 받으면 계속 지속적으로 데이터를 보내줄 수 있습니다. (Response선이 끊기지 않고 데이터 전송 가능) => return 타입 Flux
+	@CrossOrigin //js가 적용된 대로 웹페이지에서 출력되게 하고싶어용
 	@GetMapping(value = "/sender/{sender}/receiver/{receiver}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) { //데이터 여러개 리턴할거면 Flux
 		return chatRepository.mFindBySender(sender, receiver)
 				.subscribeOn(Schedulers.boundedElastic());
 	}
 	
+	@CrossOrigin
 	@PostMapping("/chat")
 	public Mono<Chat> setMsg(@RequestBody Chat chat){ //데이터 한건 리턴할거면 Mono / void 사용해도 됨 => 근데 save 데이터 잘 들어오는지 확인하고 싶어서 이렇게 작성함
 		chat.setCreatedAt(LocalDateTime.now());
-		return chatRepository.save(chat); 
+		return chatRepository.save(chat); //object를 리턴하면 자동으로 JSON 변환 (MessageConverter)
+		//(<S extends T> Mono<S> save(S entity); Mono타입으로 변환) => Mono타입은 데이터 1건 리턴, Flux타입은 데이터 여러건 리턴
 	}
 }
